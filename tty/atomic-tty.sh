f6644d4367ed2be3b3cbd20bbae1e14befa9f92a
Gerard <gerardbajona@gmail.com>
2017-01-09 00:03:58 +0100

    Added tty colors, dark mode

---
+++ tty/atomic-tty.sh
@@ -0,0 +1,19 @@
+if [[ "$TERM" = "linux" ]]; then
+    echo -en "\e]P0141A1F"
+	echo -en "\e]P1EC5252"
+	echo -en "\e]P25DA813"
+	echo -en "\e]P3C97016"
+	echo -en "\e]P43790E9"
+	echo -en "\e]P5A35AED"
+	echo -en "\e]P613AEAE"
+	echo -en "\e]P7A6B5C5"
+	echo -en "\e]P8202931"
+	echo -en "\e]P9FF9191"
+	echo -en "\e]PA9ACD68"
+    echo -en "\e]PBEDA45C"
+    echo -en "\e]PC7DBEFF"
+    echo -en "\e]PDCA96FF"
+    echo -en "\e]PE6ECFCF"
+	echo -en "\e]PF53697F"
+    clear #for background artifacting
+fi
