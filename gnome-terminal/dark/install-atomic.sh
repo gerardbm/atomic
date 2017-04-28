cb3b8c9b0e5d6dc2b215d5f8880df4e9429b1b2f
Gerard <gerardbajona@gmail.com>
2017-04-29 01:19:16 +0200

    Dropped support for gnome-terminal

--- gnome-terminal/dark/install-atomic.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/palette "#141A1F:#EC5252:#5DA813:#C97016:#3790E9:#A35AED:#13AEAE:#53697F:#202931:#FF9191:#9ACD68:#EDA45C:#7DBEFF:#CA96FF:#6ECFCF:#A6B5C5"
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/background_color "#141A1F"
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/foreground_color "#A6B5C5"
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/bold_color "#A6B5C5"
-gconftool-2 -s -t bool /apps/gnome-terminal/profiles/Default/bold_color_same_as_fg "false"
-gconftool-2 -s -t bool /apps/gnome-terminal/profiles/Default/use_theme_colors "false"
-gconftool-2 -s -t bool /apps/gnome-terminal/profiles/Default/use_theme_background "false"
